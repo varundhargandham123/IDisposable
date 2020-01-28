f531444632fbdd05ce90ab00cf166c15a4155c64
Varundhar Gandham <Varundhar_Gandham@epam.com>
2020-01-28 18:30:15 +0530

    Add project files.

---
+++ IDisposable/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace IDisposableDemo
+{
+    class IDisposableDemo : IDisposable
+    {
+        private string name;
+        private bool disposename = false;
+
+        public IDisposableDemo(string name)
+        {
+            this.name = name;
+            this.disposename = true;
+        }
+        public void display()
+        {
+            Console.WriteLine(this.name);
+        }
+        public void Dispose()
+        {
+            if (disposename)
+            {
+                name = string.Empty;
+                Console.WriteLine("disposed");
+                
+           }
+            disposename = false;
+        }
+    }
+    class DisposableDemo
+    {
+      public static void Main(string[] args)
+        {
+            IDisposableDemo student = new IDisposableDemo("varun");
+            student.display();
+            student.Dispose();
+            try
+            {
+                student.display();
+            }
+            catch (Exception)
+            {
+                Console.Write("it is disposed");
+            }
+        }
+    }
+}
